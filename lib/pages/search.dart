20a71474a333d14630582e506e56daeb847860c5
Sachitbansal <44609504+Sachitbansal@users.noreply.github.com>
2022-05-22 23:14:20 +0530

    renamed the app id

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -58,13 +58,6 @@ class _SearchState extends State<Search> {
 
     final Size size = MediaQuery.of(context).size;
 
-    print(widget.priceEnd);
-    print(widget.bathrooms);
-    print(widget.priceStart);
-    print(widget.buyOrRent);
-    print(widget.propertyType);
-    print(widget.rooms);
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -78,87 +71,124 @@ class _SearchState extends State<Search> {
               SizedBox(
                 height: MediaQuery.of(context).size.height,
                 child: StreamBuilder<QuerySnapshot>(
-                    stream: data,
-                    builder: (BuildContext context,
-                        AsyncSnapshot<QuerySnapshot> snapshot) {
-                      if (snapshot.hasError) {
-                        return const Text('Something went wrong');
-                      }
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Text('Loading');
-                      }
+                  stream: data,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Something Went Wrong.'),
+                        ),
+                      );
+                    }
+                    if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
 
-                      final List storeDocs = [];
-                      snapshot.data!.docs
-                          .map((DocumentSnapshot document) {
-                        Map a = document.data() as Map<String, dynamic>;
-                        storeDocs.add(a);
-                        a['id'] = document.id;
-                        a['collection'] = document.reference;
-                      }).toList();
+                    final List storeDocs = [];
+                    snapshot.data!.docs
+                        .map((DocumentSnapshot document) {
+                      Map a = document.data() as Map<String, dynamic>;
+                      storeDocs.add(a);
+                      a['id'] = document.id;
+                    }).toList();
 
-                      return ListView.builder(
-                        itemCount: storeDocs.length,
-                        itemBuilder: (context, index) {
-
-                          print(storeDocs[index]['id']);
-                          print('data Id');
-
-                          return Column(
-                            children: [
+                    return Expanded(
+                      child: ListView(
+                        physics: const BouncingScrollPhysics(),
+                        children: [
+                          for (var i = 0;
+                          i < storeDocs.length;
+                          i++) ...[
+                            if (storeDocs[i]['bookmark'])
                               NearbyHomes(
                                 size: size,
-                                asset: storeDocs[index]['images'],
-                                name: storeDocs[index]['title'],
-                                location: storeDocs[index]['address'],
-                                bedCount: storeDocs[index]['bedRooms'],
-                                bathCount: storeDocs[index]['bathRooms'],
-                                bookmarkIcon: storeDocs[index]['bookmark'],
+                                asset: storeDocs[i]['images'],
+                                name: storeDocs[i]['title'],
+                                location: storeDocs[i]
+                                ['address'],
+                                bedCount: storeDocs[i]
+                                ['bedRooms'],
+                                bathCount: storeDocs[i]
+                                ['bathRooms'],
+                                bookmarkIcon: storeDocs[i]
+                                ['bookmark'],
                                 bookmarkFunction: () async {
                                   CollectionReference students =
-                                      FirebaseFirestore.instance
-                                          .collection(widget.uid.toString());
+                                  FirebaseFirestore.instance
+                                      .collection(widget.uid
+                                      .toString());
 
-                                  students.doc(storeDocs[index]['id']).update({
-                                    'bookmark': !storeDocs[index]['bookmark'],
+                                  students
+                                      .doc(storeDocs[i]['id'])
+                                      .update({
+                                    'bookmark': !storeDocs[i]
+                                    ['bookmark'],
                                   }).whenComplete(() {
-                                    showSnackBar('Bookmarked',
-                                        const Duration(milliseconds: 1000));
+                                    if (!storeDocs[i]
+                                    ['bookmark']) {
+                                      showSnackBar(
+                                        'Bookmark Added',
+                                        const Duration(
+                                          milliseconds: 1000,
+                                        ),
+                                      );
+                                    } else {
+                                      showSnackBar(
+                                        'Bookmark Removed',
+                                        const Duration(
+                                          milliseconds: 1000,
+                                        ),
+                                      );
+                                    }
                                     setState(() {});
                                   });
                                 },
                                 share: () async {
                                   String generatedDeepLink =
-                                      await DynamicLinkServices
-                                          .createPropertyShareLink(
-                                              short: false,
-                                              collectionId:
-                                                  widget.uid.toString(),
-                                              docId: storeDocs[index]['id'],
-                                              imageUrl: storeDocs[index]
-                                                  ['images'][0],
-                                              propertyTitle: storeDocs[index]
-                                                  ['title']);
-                                  Share.share(generatedDeepLink);
+                                  await DynamicLinkServices
+                                      .createPropertyShareLink(
+                                      short: false,
+                                      collectionId: widget
+                                          .uid
+                                          .toString(),
+                                      docId:
+                                      storeDocs[i]
+                                      ['id'],
+                                      imageUrl: storeDocs[
+                                      i]
+                                      ['images'][0],
+                                      propertyTitle:
+                                      storeDocs[i][
+                                      'title']);
+                                  Share.share(
+                                      generatedDeepLink);
                                 },
                                 onTap: () {
                                   Navigator.push(
                                     context,
                                     MaterialPageRoute(
-                                      builder: (context) => HouseDetails(
-                                        enableEdit: true,
-                                        docId: storeDocs[index]['id'],
-                                        uid: widget.uid.toString(),
-                                      ),
+                                      builder: (context) =>
+                                          HouseDetails(
+                                            enableEdit: true,
+                                            uid: widget.uid
+                                                .toString(),
+                                            docId: storeDocs[i]
+                                            ['id'],
+                                          ),
                                     ),
                                   );
                                 },
                               ),
-                            ],
-                          );
-                        },
-                      );
-                    }),
+                          ]
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
             ],
           ),
